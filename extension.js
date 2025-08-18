9ab85375519941d1620f69e51e300d158ee37434
Lucas Roberts <rlucas7@Mac.lan>
2025-08-18 14:31:20 -0400

    switch back to cdn for chart.js renders but no plugin fire

--- extension.js
+++ extension.js
@@ -14,7 +14,7 @@ var getWebviewContent = function(barChartConfig, tsAcceptChartConfig, tsRejectCh
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Bar Chart</title>
-        <script src ="${scriptUri}"></script>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     </head>
     <body>
         <h1>Dev Data Dashboard</h1>
